958b2df8f38920f113e4670eace96a6485dc6750
Ricardo Coimbra - Eldorado <coimbra.rico@gmail.com>
2023-04-10 20:25:53 -0400

    Initial commit

---
+++ app/src/main/java/com/coimbra/lab03/MainActivity.java
@@ -0,0 +1,24 @@
+package com.coimbra.lab03;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+}
